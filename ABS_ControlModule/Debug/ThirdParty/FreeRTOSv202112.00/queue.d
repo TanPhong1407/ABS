117bef8d56c30296a560c6729cece56202b6fea1
TanPhong <phong14072001@gmail.com>
2023-07-27 10:43:56 +0700

    Add new project ABS

---
+++ ABS_ControlModule/Debug/ThirdParty/FreeRTOSv202112.00/queue.d
@@ -0,0 +1,24 @@
+ThirdParty/FreeRTOSv202112.00/queue.o: \
+ ../ThirdParty/FreeRTOSv202112.00/queue.c \
+ D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/FreeRTOS.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\freertosconfig.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\projdefs.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\portable.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\deprecated_definitions.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\portable\gcc\arm_cm3\portmacro.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\mpu_wrappers.h \
+ D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/task.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\list.h \
+ D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/queue.h \
+ d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\task.h
+D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/FreeRTOS.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\freertosconfig.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\projdefs.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\portable.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\deprecated_definitions.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\portable\gcc\arm_cm3\portmacro.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\mpu_wrappers.h:
+D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/task.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\list.h:
+D:/Learn\ school/He_thong_nhung/Embedded-main/ABS_ControlModule/ThirdParty/FreeRTOSv202112.00/include/queue.h:
+d:\learn\ school\he_thong_nhung\embedded-main\abs_controlmodule\thirdparty\freertosv202112.00\include\task.h:
